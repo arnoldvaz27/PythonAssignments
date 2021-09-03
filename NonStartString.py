5c4c4d2194022fe0805666248703d159a65d5bc5
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-09-03 21:34:52 +0530

    Project added to git

---
+++ NonStartString.py
@@ -0,0 +1,7 @@
+def non_start(first_string, second_string):
+    print(first_string[1:] + second_string[1:])
+
+
+non_start('Hello', 'There')
+non_start('java', 'code')
+non_start('shotl', 'java')
---
+++ SmallestGreatestAverageNumber.py
@@ -0,0 +1,5 @@
+numbers = [2, 4, 9, 6, 8, 1]
+secondSmallestNumber = numbers[0]
+secondGreatestNumber = numbers[4]
+avg = (secondSmallestNumber + secondGreatestNumber) / 2
+print("Output:", int(avg))
